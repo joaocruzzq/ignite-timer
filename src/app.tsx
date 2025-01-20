252cac49b8f8c0793c70258039f751d0cb6b4f62
joão pedro cruz <joaocruzzdev@gmail.com>
2025-01-20 19:57:26 -0300

    definindo rotas da aplicaçao

--- src/app.tsx
+++ src/app.tsx
@@ -3,11 +3,17 @@ import { defaultTheme } from './styles/themes/default'
 
 import { GlobalStyles } from './styles/global'
 
+import { Router } from './router'
+import { BrowserRouter } from 'react-router-dom'
+
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      
       <GlobalStyles />
-      <h1>Ignite Feed</h1>
     </ThemeProvider>
   )
 }
