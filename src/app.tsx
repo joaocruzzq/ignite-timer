5b0a468f8bd02465eca820d59c01b5325125ec42
joão pedro cruz <joaocruzzdev@gmail.com>
2025-01-20 16:52:50 -0300

    configuraçoes iniciais e definindo cores

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,13 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+
+import { GlobalStyles } from './styles/global'
+
 export function App() {
   return (
-    <h1>Ignite Feed</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
+      <h1>Ignite Feed</h1>
+    </ThemeProvider>
   )
 }
