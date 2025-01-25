f54b2890d6ebfd158800df3000b057cb66554960
joão pedro cruz <joaocruzzdev@gmail.com>
2025-01-25 11:50:15 -0300

    resetando formulario

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -33,7 +33,12 @@ export function Home() {
       }
    })
 
-   const { handleSubmit, watch, /* reset */ } = newCycleForm
+   const { handleSubmit, watch, reset } = newCycleForm
+
+   function handleCreateNewCycle(data: NewCycleFormData) {
+      createNewCycle(data)
+      reset()
+   }
 
    const task = watch('task')
 
@@ -41,7 +46,7 @@ export function Home() {
 
    return (
       <HomeContainer>
-         <form onSubmit={handleSubmit(createNewCycle)} action="">
+         <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
             <FormProvider {...newCycleForm}>
                <NewCycleForm />
             </FormProvider>
