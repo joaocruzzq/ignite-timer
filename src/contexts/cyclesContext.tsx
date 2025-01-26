833fcd1e20e28f62737c207f4cee7d68f152c5f1
joão pedro cruz <joaocruzzdev@gmail.com>
2025-01-26 17:17:08 -0300

    marcando ciclo como finalizado e incrementando switch case

--- src/contexts/cyclesContext.tsx
+++ src/contexts/cyclesContext.tsx
@@ -41,32 +41,49 @@ interface CyclesState {
 
 export function CyclesContextProvider({ children }: CyclesContextProviderProps) {
    const [cyclesState, dispatch] = useReducer((state: CyclesState, action: any) => {
-      if(action.type === 'ADD_NEW_CYCLE') {
-         return {
-            ...state,
-            cycles: [...state.cycles, action.payload.newCycle],
-            activeCycleId: action.payload.newCycle.id
-         }
+      switch(action.type) {
+         case 'ADD_NEW_CYCLE':
+            return {
+               ...state,
+               cycles: [...state.cycles, action.payload.newCycle],
+               activeCycleId: action.payload.newCycle.id
+            }
+
+         case 'INTERRUPT_CURRENT_CYCLE':
+            return {
+               ...state,
+               cycles: state.cycles.map(cycle => {
+                  if(cycle.id === state.activeCycleId) {
+                     return {...cycle, interruptedDate: new Date()}
+                  }
+   
+                  else {
+                     return cycle
+                  }
+               }),
+   
+               activeCycleId: null
+            }
+
+         case 'MARK_CURRENT_CYCLE_AS_FINISHED':
+            return {
+               ...state,
+               cycles: state.cycles.map(cycle => {
+                  if(cycle.id === state.activeCycleId) {
+                     return {...cycle, finishedDate: new Date()}
+                  }
+   
+                  else {
+                     return cycle
+                  }
+               }),
+   
+               activeCycleId: null
+            }
+
+         default:
+            return state
       }
-
-      if(action.type === 'INTERRUPT_CURRENT_CYCLE') {
-         return {
-            ...state,
-            cycles: state.cycles.map(cycle => {
-               if(cycle.id === state.activeCycleId) {
-                  return {...cycle, interruptedDate: new Date()}
-               }
-
-               else {
-                  return cycle
-               }
-            }),
-
-            activeCycleId: null
-         }
-      }
-
-      return state
    },
    
    {
@@ -85,16 +102,6 @@ export function CyclesContextProvider({ children }: CyclesContextProviderProps) 
    }
 
    function markCurrentCycleAsFinished() {
-      // setCycles(state => state.map(cycle => {
-      //    if (cycle.id === activeCycleId) {
-      //       return {...cycle, finishedDate: new Date()}
-      //    }
-
-      //    else {
-      //       return cycle
-      //    }
-      // }))
-
       dispatch({
          type: 'MARK_CURRENT_CYCLE_AS_FINISHED',
          payload: {
@@ -118,21 +125,10 @@ export function CyclesContextProvider({ children }: CyclesContextProviderProps) 
          }
       })
 
-      // setCycles((state) =>[...state, newCycle])
       setAmountSecondsPassed(0)
    }
 
    function interruptCurrentCycle() {
-      // setCycles(state => state.map(cycle => {
-      //    if (cycle.id === activeCycleId) {
-      //       return {...cycle, interruptedDate: new Date()}
-      //    }
-
-      //    else {
-      //       return cycle
-      //    }
-      // }))
-
       dispatch({
          type: 'INTERRUPT_CURRENT_CYCLE',
          payload: {
